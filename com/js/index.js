68ed7c23296e85c083e1477b98a214a971faf80d
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-01-18 23:03:27 +0900

    add sakura

--- com/js/index.js
+++ com/js/index.js
@@ -1,3 +1,11 @@
 $(function(){
-	
+	$(document).snowfall({
+		maxSize: 100,
+		maxSpeed: 15,
+		round: true,
+		//shadow: true,
+		flakeColor: "pink",
+		//collection: 'div',
+		image: 'com/img/sakura.png',
+	});
 });
